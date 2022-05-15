1d1b84fd528b6a612b5cac721f54685a1a1b5a88
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-05-15 22:38:41 +0530

    a=int(input()) for i in range(1,a+1):     for j in range(1,a+1):         b=chr(i+64)         print(b,end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,6 @@
 a=int(input())
-for i in range (1,a+1):
-    for j in range (1,a+1):
-        if i==j:
-            print('0',end='')
-        else:
-            print('x',end='')
+for i in range(1,a+1):
+    for j in range(1,a+1):
+        b=chr(i+64)
+        print(b,end=' ')
     print()
