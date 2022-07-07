8d7aa51124f5064bc312cca9e774e09936a0fe26
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-07-07 14:59:13 +0530

    def isprime(a):     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     return 1 a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if i>1:         if isprime(i):             c+=1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,13 @@
+def isprime(a):
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            return 0
+    return 1
+a=int(input())
+b=int(input())
+c=0
+for i in range(a,b+1):
+    if i>1:
+        if isprime(i):
+            c+=1
+print(c)
