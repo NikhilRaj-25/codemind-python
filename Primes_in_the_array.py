1e238ce591efdd74699be6986bd33d516fc0d303
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-07-08 19:20:37 +0530

    def prime(a):     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     return 1 a=int(input()) l=list(map(int,input().split())) c=0 for i in l:     if i>1:         if prime(i):             c+=1 print(c)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,13 @@
+def prime(a):
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            return 0
+    return 1
+a=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in l:
+    if i>1:
+        if prime(i):
+            c+=1
+print(c)
