b2e194a2a8270cf22c77e439124ba2a4ec6f96dc
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-07-09 15:16:14 +0530

    a=int(input()) l=list(map(int,input().split())) b=0 co=0 for i in l:     c=0     while i:        c+=1        i=i//10     if c>b:         b=c for i in l:     c=0     while i:        c+=1        i=i//10     if b==c:         co+=1 print(co)

---
+++ Max_digits_count.py
@@ -0,0 +1,19 @@
+a=int(input())
+l=list(map(int,input().split()))
+b=0
+co=0
+for i in l:
+    c=0
+    while i:
+       c+=1
+       i=i//10
+    if c>b:
+        b=c
+for i in l:
+    c=0
+    while i:
+       c+=1
+       i=i//10
+    if b==c:
+        co+=1
+print(co)
