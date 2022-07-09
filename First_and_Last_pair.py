ea0027acb5e9e7b0faca64c10d77586c3c967eb2
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-07-09 15:26:30 +0530

    a=int(input()) l=list(map(int,input().split())) for i in range((a//2)):     print(l[i],end=" ")     print(l[a-i-1],end=" ") if a%2!=0:     print(l[(a//2)],end=" ")     print('0')

--- First_and_Last_pair.py
+++ First_and_Last_pair.py
@@ -1,13 +1,8 @@
 a=int(input())
 l=list(map(int,input().split()))
-if a%2==0:
-    for i in range((a//2)):
-        print(l[i],end=" ")
-        print(l[a-i-1],end=" ")
-else:
-    for i in range((a//2)):
-        print(l[i],end=" ")
-        print(l[a-i-1],end=" ")
-    print(l[(a//2)],end=" ")
+for i in range((a//2)):
+    print(l[i],end=" ")
+    print(l[a-i-1],end=" ")
 if a%2!=0:
+    print(l[(a//2)],end=" ")
     print('0')
