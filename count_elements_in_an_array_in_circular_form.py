880f301e090955cea18cfa34450e72ffa5fbcbee
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-09-25 23:15:30 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in range(1,n-1):     if a[i-1]%2==0 and a[i+1]%2!=0:         c+=1;     if a[i-1]%2!=0 and a[i+1]%2==0:         c+=1; if a[n-2]%2==0 and a[0]%2!=0:     c+=1 if a[n-2]%2!=0 and a[0]%2==0:     c+=1 if a[1]%2==0 and a[n-1]%2!=0:     c+=1 if a[1]%2!=0 and a[n-1]%2==0:     c+=1 print(c)

--- count_elements_in_an_array_in_circular_form.py
+++ count_elements_in_an_array_in_circular_form.py
@@ -1,14 +1,17 @@
-a=int(input())
-l=list(map(int,input().split()))
-b=[]
-b.append(l[a-2])
-b.append(l[a-1])
+n=int(input())
+a=list(map(int,input().split()))
 c=0
-for i in l:
-    b.append(i)
-for i in range(a):
-    if b[i]%2==0 and b[i+2]%2!=0:
-        c+=1
-    elif b[i]%2!=0 and b[i+2]%2==0:
-        c+=1
+for i in range(1,n-1):
+    if a[i-1]%2==0 and a[i+1]%2!=0:
+        c+=1;
+    if a[i-1]%2!=0 and a[i+1]%2==0:
+        c+=1;
+if a[n-2]%2==0 and a[0]%2!=0:
+    c+=1
+if a[n-2]%2!=0 and a[0]%2==0:
+    c+=1
+if a[1]%2==0 and a[n-1]%2!=0:
+    c+=1
+if a[1]%2!=0 and a[n-1]%2==0:
+    c+=1
 print(c)
