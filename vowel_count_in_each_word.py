7bd5a76d652f020e9a92e18fb4b29fca3ace3752
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-10-28 13:43:45 +0530

    a=input() l=a.split() v="aeiou" d=0 for i in l:     c=0     for j in i:         if j in v:             c+=1     print(c,end=" ")

--- vowel_count_in_each_word.py
+++ vowel_count_in_each_word.py
@@ -1,9 +1,10 @@
 a=input()
-b=list(a.split( ))
-l=10
-for i in b:
+l=a.split()
+v="aeiou"
+d=0
+for i in l:
     c=0
     for j in i:
-        if j in 'aeiou':
+        if j in v:
             c+=1
-    print(c,end=' ')
+    print(c,end=" ")
