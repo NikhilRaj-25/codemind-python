c35741fd14266b4394cb9666445ae6e3135382ae
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-10-28 21:28:29 +0530

    a=input() l=a.split() b="" for i in range(len(l)-1,-1,-1):     b=l[i]     print(b[::-1],end=" ")

--- reverse_words_and_positions.py
+++ reverse_words_and_positions.py
@@ -1,12 +1,6 @@
 a=input()
-b=list(a.split( ))
-for i in range(0,len(b)):
-    c=''
-    a=b[i]
-    for j in range(len(a)-1,-1,-1):
-        c+=a[j]
-    b[i]=c
-d=[]
-for i in range(len(b)-1,-1,-1):
-    d.append(b[i])
-print(*d)
+l=a.split()
+b=""
+for i in range(len(l)-1,-1,-1):
+    b=l[i]
+    print(b[::-1],end=" ")
