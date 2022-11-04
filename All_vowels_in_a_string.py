4fc59877e16137c45924e457c92d9752dc1c85e5
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-11-04 11:33:17 +0530

    a=input() a1=['a','e','i','o','u'] a2=['A','E','I','O','U'] for i in a:     if i in a1:         a1.remove(i)     elif i in a2:         a2.remove(i) if len(a1)==0 or len(a2)==0:     print('True') else:     print('False')

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,5 +1,12 @@
-s=input()
-if 'a' in s and 'e' in s and 'i' in s and 'o' in s and 'u' in s:
+a=input()
+a1=['a','e','i','o','u']
+a2=['A','E','I','O','U']
+for i in a:
+    if i in a1:
+        a1.remove(i)
+    elif i in a2:
+        a2.remove(i)
+if len(a1)==0 or len(a2)==0:
     print('True')
 else:
     print('False')
