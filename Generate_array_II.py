cdeceecba35425c58ef27115568ea3ee30d3a35e
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-11-06 10:38:30 +0530

    n=int(input()) a=list(map(int,input().split())) c=[] for i in range(0,n,2):     for j in range(0,a[i+1]):         c.append(a[i]) print(*c)

--- Generate_array_II.py
+++ Generate_array_II.py
@@ -1,5 +1,7 @@
-a=int(input())
-l=list(map(int,input().split()))
-for i in range(0,len(l),2):
-    for j in range(0,l[i+1]):
-        print(l[i],end=" ")
+n=int(input())
+a=list(map(int,input().split()))
+c=[]
+for i in range(0,n,2):
+    for j in range(0,a[i+1]):
+        c.append(a[i])
+print(*c)
