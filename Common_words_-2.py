6608adbbc9ba50c4782340ad1daeb225bc5df430
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-11-06 09:59:37 +0530

    a=input() a=a.lower() b=input() b=b.lower() count=0 a=list(a.split(" ")) b=list(b.split(" ")) for i in a:     if i in b :         if b.count(i)==1 and a.count(i)==1:             count+=1 print(count)

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -1,17 +1,12 @@
 a=input()
+a=a.lower()
 b=input()
-l1=a.split()
-l2=b.split()
-a1=[]
-a2=[]
-for i in l1:
-    if i not in a1 and l1.count(i)==1:
-        a1.append(i)
-for i in l2:
-    if i not in a2 and l2.count(i)==1:
-        a2.append(i)
-c=0
-for i in a1:
-    if i in a2:
-        c+=1
-print(c)
+b=b.lower()
+count=0
+a=list(a.split(" "))
+b=list(b.split(" "))
+for i in a:
+    if i in b :
+        if b.count(i)==1 and a.count(i)==1:
+            count+=1
+print(count)
