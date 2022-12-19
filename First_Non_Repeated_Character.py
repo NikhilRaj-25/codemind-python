7f4123dbe98e3224cc05f9dd5478b297721d8db8
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2022-12-19 22:16:29 +0530

    for j in range(int(input())):     n=int(input())     s=input()     c=0     for i in s:         if s.count(i)==1:             print(i)             c=1             break     if c==0:         print(-1)

---
+++ First_Non_Repeated_Character.py
@@ -0,0 +1,11 @@
+for j in range(int(input())):
+    n=int(input())
+    s=input()
+    c=0
+    for i in s:
+        if s.count(i)==1:
+            print(i)
+            c=1
+            break
+    if c==0:
+        print(-1)
